0c867897925ed386c7963de73ea3691bf05b6383
Gleb <gleblubiskin@gmail.com>
2023-02-01 20:42:10 +0300

    main - Small changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
+// Библиотеки
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+// App
+import {App} from "@pages/app";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
---
+++ src/pages/app/App.tsx
@@ -0,0 +1,12 @@
+// Библиотеки
+import React from "react";
+
+/**
+ * Главный компонент приложения
+ * @constructor
+ */
+export const App: React.FC = () => {
+    return (
+        <h1>Hello Ali!</h1>
+    )
+}
